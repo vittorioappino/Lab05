477088681235f5943504444a6661995191418433
tattommasi <tatiana.tommasi@polito.it>
2021-04-16 19:42:52 +0200

    upload soluzione

--- Lab05_Anagrammi/src/main/java/it/polito/tdp/anagrammi/EntryPoint.java
+++ Lab05_Anagrammi/src/main/java/it/polito/tdp/anagrammi/EntryPoint.java
@@ -1,7 +1,7 @@
 package it.polito.tdp.anagrammi;
 
 import javafx.application.Application;
-import static javafx.application.Application.launch;
+import it.polito.tdp.anagrammi.model.AnagrammiModel;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -12,12 +12,19 @@ public class EntryPoint extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
-        
+    	
+    	FXMLController controller;
+    	
+    	FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Scene.fxml"));
+        Parent root = loader.load();
         Scene scene = new Scene(root);
-        scene.getStylesheets().add("/styles/Styles.css");
-        
-        stage.setTitle("JavaFX and Maven");
+
+        controller = loader.getController();
+
+        AnagrammiModel model = new AnagrammiModel();
+        controller.setModel(model);
+            
+        stage.setTitle("Calcola Anagrammi");
         stage.setScene(scene);
         stage.show();
     }
